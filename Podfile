97238deccb1318ab34f0edaabffc9d41cc841fdc
vladislava_zabolonkova <velga4323@gmail.com>
2014-11-05 22:33:35 +0200

    pods added

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, "6.1"
+
+source 'https://github.com/CocoaPods/Specs.git'
+
+pod 'SocketRocket', '0.3.1-beta2'
+
--- TestiOSDeveloperApp/ViewController.m
+++ TestiOSDeveloperApp/ViewController.m
@@ -19,6 +19,11 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)dealloc
+{
+    [super dealloc];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
